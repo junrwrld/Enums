458fe1f21f715a05a2dda38f5c5e52a89808c454
João Junior <batistasousajoao@gmail.com>
2023-12-03 13:49:14 -0300

    Add project files.

---
+++ Course/Program.cs
@@ -0,0 +1,32 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order{
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            //Declarando o tipo ENUM em tipo STRING
+            string txt = OrderStatus.PendingPayment.ToString();
+            //Declarando STRING para tipo ENUM
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            //IMPORTANTE RESSALTAR QUE PRECISA SER O NOME EXATO, NÃO PODE FALTAR CARACTERE
+            //EXEMPLO: "Delivere"
+
+            //RESULTADO:
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
